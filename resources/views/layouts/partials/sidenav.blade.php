1496df3358c1ef5fe2d2fc7b4b2fcbe80c4d69f7
AdrianID <adrianfrizna14@gmail.com>
2025-03-17 13:44:30 +0700

    fix: Correct image path in sidenav for proper logo display

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -2,7 +2,7 @@
     <div class="h-25">
         <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close=""></i>
         <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700 text-center" href="{{ route('dashboard') }}">
-            <img src="./assets/logopdam.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand mx-auto" alt="main_logo">
+            <img src="/assets/logopdam.png" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand mx-auto" alt="main_logo">
         </a>
     </div>
 
